9ece92f9f1c73b7d8e469248a2855485f6dcda53
Rafael <khachikyan38@gmail.com>
2021-12-05 00:57:35 +0300

    js is ready

--- README.md
+++ README.md
@@ -16,11 +16,7 @@ This is a solution to the [Interactive Pricing Component Challenge on Frontend M
 
 ### The challenge
 
-Your challenge is to build out this interactive pricing component and get it looking as close to the design as possible.
-
-You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
-
-Your users should be able to:
+User should be able to:
 
 - View the optimal layout for the app depending on their device's screen size
 - See hover states for all interactive elements on the page
@@ -42,6 +38,8 @@ Your users should be able to:
 - Semantic HTML5 markup
 - SCSS custom properties
 - CSS Flexbox
+- CSS Grid
+- Vanilla JS
 - Mobile-first workflow
 
 ## Author
--- js/script.js
+++ js/script.js
@@ -5,56 +5,53 @@ const price = document.querySelector(".js-price");
 const discount = document.querySelector(".js-discount");
 const button = document.querySelector(".js-button");
 
-console.log(checkbox.checked === false);
-
 const pageviews = ["10K", "50K", "100K", "500K", "1M"];
 const dollar = [8, 12, 16, 24, 36];
+const dollarSale = dollar.slice().map((el) => el - el * 0.25);
 
-const changeColor = () => {
-  slider.addEventListener("input", () => {
-    let value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
-    let color = `linear-gradient(90deg,
-          rgb(165, 243, 235) ${value}%,
-          rgb(205, 215, 238) ${value}%)`;
-    slider.style.backgroundImage = color;
-  });
-};
-
-const calculate = () => {
-  slider.addEventListener("input", () => {
-    view.textContent = pageviews[slider.value - 1];
-    price.textContent = `$${dollar[slider.value - 1]}.00`;
-  });
-};
-
-const sale = () => {
-  checkbox.addEventListener("click", () => {
-    if (checkbox.checked) {
-      discount.textContent = `-${discount.textContent}`;
-      price.textContent = `$${dollar[slider.value - 1] / 2}.00`;
-    } else {
-      price.textContent = `$${dollar[slider.value - 1]}.00`;
-    }
-  });
-};
-
-const reset = () => {
-  slider.value = 3;
-  view.textContent = pageviews[slider.value - 1];
-  price.textContent = `$${dollar[slider.value - 1]}.00`;
+const changeColor = function () {
   let value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
   let color = `linear-gradient(90deg,
           rgb(165, 243, 235) ${value}%,
           rgb(205, 215, 238) ${value}%)`;
   slider.style.backgroundImage = color;
+};
+
+const calculate = function () {
+  view.textContent = pageviews[slider.value - 1];
+  price.textContent = isChecked
+    ? `$${dollar[slider.value - 1] - dollar[slider.value - 1] * 0.25}.00`
+    : `$${dollar[slider.value - 1]}.00`;
+};
+
+let isChecked = false;
+
+const sale = function () {
+  if (checkbox.checked) {
+    isChecked = true;
+    price.textContent = `$${
+      dollar[slider.value - 1] - dollar[slider.value - 1] * 0.25
+    }.00`;
+  } else {
+    isChecked = false;
+    price.textContent = `$${dollar[slider.value - 1]}.00`;
+  }
+};
+
+const reset = function () {
+  isChecked = false;
+  slider.value = 3;
+  view.textContent = pageviews[slider.value - 1];
+  price.textContent = `$${dollar[slider.value - 1]}.00`;
+  let color = `linear-gradient(90deg,
+          rgb(165, 243, 235) 50%,
+          rgb(205, 215, 238) 50%)`;
+  slider.style.backgroundImage = color;
   checkbox.checked = false;
 };
 
-button.addEventListener("click", () => {
-  reset();
-});
-
-changeColor();
-calculate();
-sale();
-reset();
+button.addEventListener("click", reset);
+slider.addEventListener("input", calculate);
+slider.addEventListener("input", changeColor);
+checkbox.addEventListener("click", sale);
+window.addEventListener("load", reset);
